b8e21d133a3f787741867dbc866cbb4583f72aa5
PoeticBeam <9thadetunji@gmail.com>
2023-03-16 23:02:37 -0700

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
+ *Write a C program that
+ *prints exactly "Programming is like building a
+ *multilingual puzzle, followed by a new line.t
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
