81a890d2fca045afd191185820afd0775e9cfee5
PoeticBeam <9thadetunji@gmail.com>
2023-03-21 19:23:52 -0700

    created code toprint alpahabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+int a = 97;
+
+while (a <= 122)
+{
+putchar(a);
+a = a + 1;
+}
+return;
+}
+
