756830e16cd653ec4984644a1268b72dbc86af18
PoeticBeam <9thadetunji@gmail.com>
2023-03-22 01:03:58 +0000

    code to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+   * main - this is the entry
+    * Description: Write a program that prints _putchar,
+     *  followed by a new line.
+      * Return: 0 (success)
+       */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
