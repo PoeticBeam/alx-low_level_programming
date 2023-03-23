bcc55c7c1468ff853c1009c60ed86794c04fc9e8
PoeticBeam <9thadetunji@gmail.com>
2023-03-23 22:01:50 +0000

    function multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: receives value
+ * @b: receives assigned value
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
