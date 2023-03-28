ec5b20d6a2402de2492fec98ff4c35351dbbb791
PoeticBeam <9thadetunji@gmail.com>
2023-03-28 00:35:08 +0000

    added function to reset pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates value of pointer to 98
+ * @n: pointer parameter
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return;
+}
