b84e001df6e67ee1ab44d22fd94985895bfe4349
PoeticBeam <9thadetunji@gmail.com>
2023-03-28 20:49:50 -0700

    string from source to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string from source to destination
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
