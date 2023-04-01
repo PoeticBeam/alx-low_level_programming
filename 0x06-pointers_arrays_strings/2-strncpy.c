b0b4ce0d15aaafa16b8c8eb75c605c0886785f78
PoeticBeam <9thadetunji@gmail.com>
2023-04-01 01:08:58 +0000

    copies n char from src to dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,18 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n && src[i] != '\0')
+	while (src[i] != '\0' && n > 0)
 	{
 		dest[i] = src[i];
 		i++;
+		n--;
 	}
-	dest[i] = '\0';
+
+	while (n > 0)
+	{
+		dest[i] = '\0';
+		n--;
+	}
+
 	return (dest);
 }
