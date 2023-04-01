d80ecd8d98a41b9551efd1825ebe90e641954990
PoeticBeam <9thadetunji@gmail.com>
2023-04-01 01:31:33 +0000

    compares two strings by value

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compares two strings per value of characters
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0 for same, 1 for greater, -1 for less
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	i++;
+	if (s1[i] > s2[i])
+	return (1);
+
+	else if (s1[i] < s2[i])
+	return (-1);
+
+	else
+	return (0);
+}
