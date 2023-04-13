1e4458c940c1984ef310a92713ddffc1399c3d15
PoeticBeam <9thadetunji@gmail.com>
2023-04-13 13:14:02 +0000

    malloc task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer to newly allocated space in memory
+ * @str: old string passed to function
+ *
+ * Return: NULL if no character or space unavailable, copy str2 on success
+ */
+
+char *_strdup(char *str)
+{
+	char *str2;
+	int i, r;
+
+	if (str == NULL || str == 0)
+		return (NULL);
+	for (i = 0; i != '\0'; i++)
+		str2 = (char *)malloc(sizeof(char) * (i + 1));
+	if (str2 == NULL)
+		return (NULL);
+	for (r = 0; str[r]; r++)
+		str2[r] = str[r];
+	return (str2);
+	free(str2);
+}
