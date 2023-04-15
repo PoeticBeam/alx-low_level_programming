bdcd5b5113e408a48034a3521f7dfe2e084e1af3
PoeticBeam <9thadetunji@gmail.com>
2023-04-15 19:50:28 +0000

    9-strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,6 +10,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
