a606d4acd40015503c128f6ee03088e3cc5a589e
PoeticBeam <9thadetunji@gmail.com>
2023-04-16 18:54:05 +0000

    naturals

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all naturals form n to 98
+ * @n: start from here
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
+		}
+	}
+	printf("\n");
+}
