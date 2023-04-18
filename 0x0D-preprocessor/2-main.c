792e5a65d476c9f2e844c5cf6ff72022ad8fe071
PoeticBeam <9thadetunji@gmail.com>
2023-04-18 01:53:51 +0000

    __FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,11 @@
 /**
  * main - check the code
  * code prints name of source code file
- * Return: nothing
+ * Return: 0 on success
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
+	return (0);
 }
