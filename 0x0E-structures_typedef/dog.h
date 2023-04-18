3d499eb1250142b64641bf83ba95885f0aa6f7c2
PoeticBeam <9thadetunji@gmail.com>
2023-04-18 05:42:12 -0700

    sandbox

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,16 @@
  * @owner: dog owner
  *
  * my_dog - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
+} my_dog, dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
