6ba6b6fec32a193a23bd70922358f39ad39adc85
PoeticBeam <9thadetunji@gmail.com>
2023-04-20 00:44:51 +0000

    CODE task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,5 +12,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	action(array[size]);
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
