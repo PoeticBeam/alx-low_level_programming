043f3624e057728848fe7851cb30c1487b61825d
PoeticBeam <9thadetunji@gmail.com>
2023-04-30 14:05:44 +0000

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal line on the terminal
+ * @n: no of times to print \
+ *
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
