8d1b5ba99508770ea504841792f2e612468e0515
PoeticBeam <9thadetunji@gmail.com>
2023-04-30 12:08:04 +0000

    failed commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,3 +1,15 @@
-#include "main.h"
+#include <stdio.h>
 
-int _isupper(int c);
+/**
+ * _isupper - function returns 1 (success) if uppercase
+ * @c: integer c receiving assigned value
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
